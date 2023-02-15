9f65aa4536f4d31dfb39323e20ec22afb5803f81
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-02-15 17:36:36 +0100

    Update Berniebron.java

--- Berniebron.java
+++ Berniebron.java
@@ -3,22 +3,26 @@ class Base {
     protected static int iAge = 3;
 }
 
-public class Berniebron extends Base {
+public class Berniebron extends Base 
+{
     public int iAge = 10; // przypisuje wartość 10 do parametru iAge w obiekcie typu Berniebron
                           // metoda go() nie korzysta z tego parametru, 
                           // gdyż w metodzie podane jest aby korzystała z parametru klasy Base (Base.iAge)
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Berniebron bb = new Berniebron();
         bb.go();
     }
-    public void go(){
+    public void go()
+    {
         int iOutput = calc(2);
         System.out.println(iOutput * Base.iAge); // Rozwiązanie: błąd kompilacji
         // błąd kompilacji występuje ponieważ parametr iAge jest prywatny w klasie Base,
         // jeżeli parametr byłby publiczny lub chroniony(protected) to byłby dostępny dla innych klas,
         // ponieważ jest prywatny, nie jest dziedziczony przez klasę Berniebron
     }
-    public int calc(int iAge){
+    public int calc(int iAge)
+    {
         return (this.iAge * iAge); // calc korzysta z parametrów 10 i 2 -> 10 * 2
                                    // gdyż liczba 10 jest przypisana do obiektów typu Berniebron
     }
