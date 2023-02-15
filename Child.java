6cff2e0a0f01ce817c5a2473e3a920d545275cee
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-02-15 17:39:34 +0100

    Update Child.java

--- Child.java
+++ Child.java
@@ -1,25 +1,32 @@
 // 16. Znajdź, omów i popraw błędy w poniższym programie, oraz zaproponuj poprawny program (idk co autorka miała na myśli -
 // - w programie jest tylko jeden błąd)
-class Parent {
+class Parent 
+{
     private String name;
 
-    Parent(String name){
+    Parent(String name)
+    {
         this.name = name;
     }
-    String getName(){
+    String getName()
+    {
         return name;
     }
-    void setName(String name){
+    void setName(String name)
+    {
         this.name = name;
     }
 }
 
-class Child extends Parent {
-    public Child(){
+class Child extends Parent 
+{
+    public Child()
+    {
         //super(); // błąd - trzeba podać parametr typu String, gdyż konstruktor nadrzędny (Parent) go oczekuje
         super("Billy");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Child child = new Child();
         child.setName("Benny");
         System.out.println(child.getName());
